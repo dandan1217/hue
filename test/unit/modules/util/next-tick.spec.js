a1242b868734360bb93b951579b955822b78ea3a
luskylr <luskylr@gmail.com>
2017-07-01 10:07:17 +0800

    rewrite nextTick testcase

--- test/unit/modules/util/next-tick.spec.js
+++ test/unit/modules/util/next-tick.spec.js
@@ -1,4 +1,4 @@
-import { nextTick } from 'core/util/env'
+import nextTick from 'core/observer/helpers/nexttick'
 
 describe('nextTick', () => {
   it('accepts a callback', done => {
@@ -6,29 +6,27 @@ describe('nextTick', () => {
   })
 
   it('returns undefined when passed a callback', () => {
-    expect(nextTick(() => {})).toBeUndefined()
+    expect(nextTick(() => { })).toBeUndefined()
   })
 
-  if (typeof Promise !== 'undefined') {
-    it('returns a Promise when provided no callback', done => {
-      nextTick().then(done)
-    })
+  it('returns a Promise when provided no callback', done => {
+    nextTick().then(done)
+  })
 
-    it('returns a Promise with a context argument when provided a falsy callback and an object', done => {
-      const obj = {}
-      nextTick(undefined, obj).then(ctx => {
-        expect(ctx).toBe(obj)
-        done()
-      })
+  it('returns a Promise with a context argument when provided a falsy callback and an object', done => {
+    const obj = {}
+    nextTick(undefined, obj).then(ctx => {
+      expect(ctx).toBe(obj)
+      done()
     })
+  })
 
-    it('returned Promise should resolve correctly vs callback', done => {
-      const spy = jasmine.createSpy()
-      nextTick(spy)
-      nextTick().then(() => {
-        expect(spy).toHaveBeenCalled()
-        done()
-      })
+  it('returned Promise should resolve correctly vs callback', done => {
+    const spy = jasmine.createSpy()
+    nextTick(spy)
+    nextTick().then(() => {
+      expect(spy).toHaveBeenCalled()
+      done()
     })
-  }
+  })
 })
