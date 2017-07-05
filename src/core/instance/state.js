8cd050f8137e2b56d44d79028e8182780ef60c4a
luskylr <luskylr@gmail.com>
2017-07-05 09:14:08 +0800

    add state.js stage 2

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -22,6 +22,64 @@ function initData(vm) {
   observe(data, true)
 }
 
+const computedWatchersOptions = { lazy: true }
+
+function initComputed(vm, computed) {
+  const watchers = vm._computedWatchers = Object.create(null)
+  for (const key in computed) {
+    const userDef = computed[key]
+    let getter = typeof userDef === 'function' ? userDef : userDef.get
+    if (getter === undefined) {
+      throw new Error(`No getter function for computed property "${key}".`)
+    }
+  }
+  watchers[key] = new Watcher(vm, getter, noop, computedWatchersOptions)
+  if (!(key in vm)) {
+    defineComputed(vm, key, userDef)
+  } else {
+    throw new Error(`The computed property "${key}" is already defined`, vm)
+  }
+}
+
+const sharedPropertyDefinition = {
+  enumerable: true,
+  configurable: true,
+  get: noop,
+  set: noop
+}
+
+export function defineComputed(target, key, userDef) {
+  sharedPropertyDefinition.get = sharedPropertyDefinition.set = noop
+  if (typeof userDef === 'function') {
+    sharedPropertyDefinition.get = createComputedGetter(key)
+  } else {
+    if (userDef.get) {
+      sharedPropertyDefinition.get = userDef.cache !== false
+        ? createComtedGetter(key)
+        : userDef.get
+    }
+    if (userDef.set) {
+      sharedPropertyDefinition.set = userDef.set
+    }
+  }
+  Object.defineProperty(target, key, sharedPropertyDefinition)
+}
+
+function createComputedGetter(key) {
+  return function computedGetter() {
+    const watcher = this._computedWatchers && this._computedWatchers[key]
+    if (watcher) {
+      if (watcher.dirty) {
+        watcher.evaluate()
+      }
+      if (Dep.target) {
+        watcher.depend()
+      }
+      return watcher.value
+    }
+  }
+}
+
 function initMethods(vm, methods) {
   const props = vm.$options.props
   for (const key in methods) {
@@ -34,8 +92,6 @@ function initMethods(vm, methods) {
     }
   }
 }
-
-const computedWatchersOptions = { lazy: true }
 
 function initWatch(vm, watch) {
   for (const key in watch) {
