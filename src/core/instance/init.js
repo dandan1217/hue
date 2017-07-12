0ce93d94afdff1689a50f74daefaf6b3b9357a88
luskylr <luskylr@gmail.com>
2017-07-12 08:56:50 +0800

    add init.js stage 2

--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -23,3 +23,18 @@ export function initMixin(Hue) {
     vm.$mount(vm.$options.el)
   }
 }
+
+function initInternalComponent(vm, options) {
+  const opts = vm.$options = Object.create(vm.constructor.options)
+  opts.parent = options.parent
+  opts._parentVnode = options._parentVnode
+  opts._parentListeners = options._parentListeners
+  opts._renderChildren = options._renderChildren
+  opts._componentTag = options._componentTag
+  opts._parentElm = options._parentElm
+  opts._refElm = options._refElm
+  if(options.render) {
+    opts.render = options.render
+    opts.staticRenderFns = options.staticRenderFns
+  }
+}
